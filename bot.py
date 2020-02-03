d84413c448dd8bfb12d3633cb91994993271260e
Афанасьев Виталий <17476018U@MacBook-Pro-internet-5.local>
2020-02-03 12:27:52 +0300

    test buttons

--- bot.py
+++ bot.py
@@ -1,20 +1,29 @@
 import os
 import time
 
-import telebot
+import telebot as tb
 
 
-bot = telebot.TeleBot(os.environ['TOKEN'])
+bot = tb.TeleBot(os.environ['TOKEN'])
 
 
 @bot.message_handler(commands=['authorization'])
 def handle_authorization(message):
-    bot.send_message(message.from_user.id, 'you press authorization')
+    bot.send_message(message.from_user.id, 'Отправьте, пожалуйста, ваш номер полиса')
+    bot.send_message(message.from_user.id, 'Отправьте, пожалуйста, вашу дату рождения в формате ДД ММ ГГГГ')
+    keyboard = [[tb.InlineKeyboardButton("Записаться", callback_data='1')]]
+    markup = tb.InlineKeyboardMarkup(keyboard)
+    bot.send_message(message.from_user.id, 'Пожалуйста выберите:', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     bot.send_message(message.from_user.id, 'test')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def  test_callback(call):
+    bot.send_message(call.from_user.id, str(call))
 
 
 if __name__ == '__main__':
