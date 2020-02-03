3b3803ffbd1fb24bd4a83585db538445003ecce9
Афанасьев Виталий <17476018U@MacBook-Pro-internet-5.local>
2020-02-03 13:31:51 +0300

    test commit

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import time
 
 import telebot as tb
 
+from services import Emias
+
 
 bot = tb.TeleBot(os.environ['TOKEN'])
 
@@ -10,11 +12,21 @@ bot = tb.TeleBot(os.environ['TOKEN'])
 @bot.message_handler(commands=['authorization'])
 def handle_authorization(message):
     bot.send_message(message.from_user.id, 'Отправьте, пожалуйста, ваш номер полиса')
-    bot.send_message(message.from_user.id, 'Отправьте, пожалуйста, вашу дату рождения в формате ДД ММ ГГГГ')
-    # keyboard = [[tb.InlineKeyboardButton("Записаться", callback_data='1')]]
-    # markup = tb.InlineKeyboardMarkup(keyboard)
+    bot.send_message(message.from_user.id, 'Отправьте, пожалуйста, вашу дату рождения в формате ГГГГ-ММ-ДД')
+
+
+@bot.message_handler(content_types=['text'], func=lambda massege: len(massege.text) == 11)
+def handle_oms(message):
+    omsNumber = massege.text
+    bot.send_message(message.from_user.id, f'oms: {omsNumber }')
+
+
+@bot.message_handler(content_types=['text'], func=lambda massege: len(massege.text) == 10)
+def handle_date(message):
+    birthDate = massege.text
+    bot.send_message(message.from_user.id, f'birh: {omsNumber }')
     markup = tb.types.InlineKeyboardMarkup()
-    markup.add(tb.types.InlineKeyboardButton("Записаться", callback_data='1'))
+    markup.add(tb.types.InlineKeyboardButton("Записаться", callback_data='start'))
     bot.send_message(message.from_user.id, 'Пожалуйста выберите:', reply_markup=markup)
 
 
@@ -23,9 +35,46 @@ def handle_text(message):
     bot.send_message(message.from_user.id, 'test')
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def  test_callback(call):
-    bot.send_message(call.from_user.id, str(call))
+@bot.callback_query_handler(func=lambda call: call.data == 'start')
+def handle_departments(call):
+    markup = tb.types.InlineKeyboardMarkup()
+
+    for departament in Emias.deparments(omsNumber, birthDate):
+        markup.add(tb.types.InlineKeyboardButton(departament['name'], callback_data='dep'+departament['code']))
+
+    bot.send_message(message.from_user.id, 'Пожалуйста выберите:', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('dep'))
+def handle_doctors(call):
+    markup = tb.types.InlineKeyboardMarkup()
+
+    code = call.data[3:]
+    for doctor in Emias.doctors(omsNumber, birthDate, code):
+        markup.add(tb.types.InlineKeyboardButton(doctor['name'], callback_data='doc'+str(doctor['id'])))
+
+    bot.send_message(message.from_user.id, 'Пожалуйста выберите:', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('doc'))
+def handle_doctors(call):
+    markup = tb.types.InlineKeyboardMarkup()
+
+    id = int(call.data[3:])
+    doctor = Emias.schedule(omsNumber, birthDate, id)
+    for day in doctor['scheduleOfDay']:
+        for row_schedule in day['scheduleBySlot'][0]['slot']:
+            markup.add(tb.types.InlineKeyboardButton(row_schedule['startTime'], callback_data='sch'+str(doctor['id'])))
+
+    bot.send_message(message.from_user.id, 'Пожалуйста выберите:', reply_markup=markup)
+
+
+
+# @bot.callback_query_handler(func=lambda call: True)
+# def test_callback(call):
+#     bot.send_message(call.from_user.id, str(call))
+#     departaments = 
+#     return
 
 
 if __name__ == '__main__':
---
+++ services.py
@@ -0,0 +1,114 @@
+import requests
+
+
+class Emias:
+    @staticmethod
+    def deparments(omsNumber: str, birthDate: str) -> list:
+        """
+        :param omsNumber: len(omsNumber) == 11 and omsNumber.isdigit() == True
+        :param birthDate: YYYY-MM-DD
+        :return: pass
+        """
+
+        url = "https://emias.info/api/new/eip2/?getSpecialitiesInfo"
+        data = {
+            "jsonrpc": "2.0",
+            "id": "",
+            "method": "getSpecialitiesInfo",
+            "params": {"omsNumber": omsNumber, "birthDate": birthDate,},
+        }
+
+        response = requests.post(url, json=data)
+        result = response.json()["result"]
+        return result
+
+    @staticmethod
+    def doctors(omsNumber: str, birthDate: str, specialityId: str) -> list:
+        """
+        :param omsNumber: len(omsNumber) == 11 and omsNumber.isdigit() == True
+        :param birthDate: YYYY-MM-DD
+        :param specialityId:  specialityId.isdigit() == True
+        :return: pass
+        """
+
+        url = "https://emias.info/api/new/eip2/?getDoctorsInfo"
+        data = {
+            "jsonrpc": "2.0",
+            "id": "",
+            "method": "getDoctorsInfo",
+            "params": {
+                "omsNumber": omsNumber,
+                "birthDate": birthDate,
+                "specialityId": specialityId,
+            },
+        }
+
+        response = requests.post(url, json=data)
+        result = response.json()["result"]
+        return result
+
+    @staticmethod
+    def schedule(omsNumber: str, birthDate: str, availableResourceId: int) -> dict:
+        """
+        :param omsNumber: len(omsNumber) == 11 and omsNumber.isdigit() == True
+        :param birthDate: YYYY-MM-DD
+        :param availableResourceId: pass
+        :return: pass
+        """
+
+        url = "https://emias.info/api/new/eip2/?getAvailableResourceScheduleInfo"
+        data = {
+            "jsonrpc": "2.0",
+            "id": "",
+            "method": "getAvailableResourceScheduleInfo",
+            "params": {
+                "omsNumber": omsNumber,
+                "birthDate": birthDate,
+                "availableResourceId": availableResourceId,
+            },
+        }
+
+        response = requests.post(url, json=data)
+        result = response.json()["result"]
+        return result
+
+    @staticmethod
+    def create_appointment(
+        omsNumber: str,
+        birthDate: str,
+        availableResourceId: int,
+        complexResourceId: int,
+        receptionTypeId: str,
+        startTime: str,
+        endTime: str,
+    ) -> dict:
+        """
+        :param omsNumber: len(omsNumber) == 11 and omsNumber.isdigit() == True
+        :param birthDate: YYYY-MM-DD
+        :param availableResourceId: pass
+        :param complexResourceId: pass
+        :param receptionTypeId: receptionTypeId.isdigit() == True
+        :param startTime: YYYY-MM-DDTHH:MM:SS+03:00
+        :param endTime: YYYY-MM-DDTHH:MM:SS+03:00
+        :return: pass
+        """
+
+        url = "https://emias.info/api/new/eip2/?createAppointment"
+        data = {
+            "jsonrpc": "2.0",
+            "id": "",
+            "method": "createAppointment",
+            "params": {
+                "omsNumber": omsNumber,
+                "birthDate": birthDate,
+                "availableResourceId": availableResourceId,
+                "complexResourceId": complexResourceId,
+                "receptionTypeId": complexResourceId,
+                "startTime": startTime,
+                "endTime": endTime,
+            },
+        }
+
+        response = requests.post(url, json=data)
+        result = response.json()["result"]
+        return result
