df973cbfd0a1a22abdf70b7781bc3a148135133e
Афанасьев Виталий <17476018U@MacBook-Pro-internet-5.local>
2020-02-03 11:14:04 +0300

    drop prints

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ import time
 
 import telebot
 
-print('init')
-print(os.environ['token'])
+
 bot = telebot.TeleBot(os.environ['token'])
 
 
@@ -16,7 +15,6 @@ def handle_text(message):
 if __name__ == '__main__':
     while True:
         try:
-            print('start')
             bot.polling(none_stop=True, timeout=0)
         except Exception as e:
             time.sleep(3)
